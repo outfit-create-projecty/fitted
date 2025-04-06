e817f30e4d3e2f59c5d138f8ff6831502dc280cb
Emanuel Medina <emanuelm1568@gmail.com>
2025-04-06 06:04:15 -0400

    Made changes to the footer

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -10,7 +10,7 @@ export default function Footer(props: { user?: Session['user'] }) {
         <div className='flex flex-col w-[calc(40vw-32px)] h-full'>
             <div className="flex flex-row items-center"><WashingMachine className="size-6" /><span className="text-primary-foreground text-3xl font-bold">Fit</span><span className="text-secondary text-3xl font-bold">ted</span></div>
             <p className="text-sm font-light pr-16 pt-4">Choose your style.</p>
-            <p className="mt-auto text-sm font-extralight">Copyright 2025 Fitted</p>
+            <p className="mt-auto text-sm font-extralight">Copyright © 2025 Fitted</p>
         </div>
         <div className='flex flex-col flex-1 h-full'>
             <h1 className="text-sm font-bold text-m-accent">Overview</h1>
